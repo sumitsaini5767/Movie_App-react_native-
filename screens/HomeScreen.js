7ef328932470c479598f7619bba57b52c0da4073
sumit <sumitsaini576@gmail.com>
2024-02-15 10:50:01 +0530

    .

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+}
